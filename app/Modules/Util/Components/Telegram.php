95f87744017a5a6ae679c8c5780db0d303c89c48
Sasha <xsashaxsashax@gmail.com>
2019-03-21 17:42:07 +0300

    telegram proxy

--- app/Modules/Util/Components/Telegram.php
+++ app/Modules/Util/Components/Telegram.php
@@ -11,40 +11,41 @@ namespace Modules\Util\Components;
 
 class Telegram
 {
-    public $secret = ""; // FILL THIS
+    public $secret = ''; // FILL THIS
 
     public $chat = ''; // AND THIS
 
-    public $baseUrl = "https://api.telegram.org/bot";
+    public $baseUrl = 'https://api.telegram.org/bot';
 
-    public function send($text)
-    {
-        if ($this->chat && $this->secret) {
-            $this->request('sendMessage', [
-                'parse_mode' => "HTML",
-                'disable_web_page_preview' => 'true',
-                'chat_id' => $this->chat,
-                'text' => $text
-            ]);
-        }
-    }
+    public $proxy = 'http://51.15.220.235:8212';
 
     public function getUpdates()
     {
         // Go to: https://api.telegram.org/bot{bot}/getUpdates
     }
 
+    public function send($text)
+    {
+        $this->request('sendMessage', [
+            'parse_mode' => "HTML",
+            'disable_web_page_preview' => 'true',
+            'chat_id' => $this->chat,
+            'text' => $text
+        ]);
+    }
+
     public function request($url, $data = [])
     {
         $url = $this->baseUrl . $this->secret . '/' . $url . '?' . http_build_query($data);
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, 'https://rclass.pro/pro/tg');
+        curl_setopt($ch, CURLOPT_URL, $url);
+        if ($this->proxy) {
+            curl_setopt($ch, CURLOPT_PROXYTYPE, CURLPROXY_SOCKS5);
+            curl_setopt($ch, CURLOPT_PROXY,  $this->proxy);
+        }
         curl_setopt($ch, CURLOPT_HEADER, false);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, [
-            'url' => $url,
-            'key' => "d23d46d6-8bf8-11e8-9eb6-529269fb1459"
-        ]);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 
         $result = curl_exec($ch);
