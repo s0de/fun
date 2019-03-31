deaa1f38df61f62da1a3aab8a1eeda68fdc4dbde
Sasha <websoda@ya.ru>
2019-03-31 15:40:33 +0000

    require map.js

--- static/js/app.js
+++ static/js/app.js
@@ -8,6 +8,7 @@ require('./common/links.js');
 require('./common/main.js');
 require('./common/pagination.js');
 require('./common/respond.js');
+require('./common/map.js');
 
 require('./field/phone.js');
 require('../components/smart-tabs/smart-tabs');
